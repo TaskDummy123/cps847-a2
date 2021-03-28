8b8fa63412018408181b982aad01242373bb2a59
BrianBNguyen <59449776+BrianBNguyen@users.noreply.github.com>
2021-03-28 16:48:38 -0400

    change back to original
    reverted previous changes

--- app.py
+++ app.py
@@ -1,82 +1,13 @@
-#!flask/bin/python
-from flask import Flask, jsonify
+  
+from flask import Flask, render_template
+
 app = Flask(__name__)
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
-@app.route('/io', methods=['GET'])
-def get_tasks():
-
-    if 1 == 1:
-        print('Yes')
-    else:
-        print('no')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-        print('Yes')
-
-    return jsonify({'tasks': tasks})
 
 @app.route('/')
-def index():
-    return "Hello, World!"
+def hello_world():
+    return render_template('index.html')
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
