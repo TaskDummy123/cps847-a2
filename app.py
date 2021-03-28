e0c28df7835014e9aa6a8c69ec8279263dfd1417
BrianBNguyen <59449776+BrianBNguyen@users.noreply.github.com>
2021-03-28 16:26:28 -0400

    wrong python file
    the unit test file was suppose to be using this one

--- app.py
+++ app.py
@@ -1,11 +1,82 @@
-  
-from flask import Flask, render_template
-
+#!flask/bin/python
+from flask import Flask, jsonify
 app = Flask(__name__)
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/io', methods=['GET'])
+def get_tasks():
+
+    if 1 == 1:
+        print('Yes')
+    else:
+        print('no')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+        print('Yes')
+
+    return jsonify({'tasks': tasks})
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
